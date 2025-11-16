5a8455f98e9ea09a6554eb53b6e4d80a3239e15c
PawelekKlocek <116948265+PawelekKlocek@users.noreply.github.com>
2025-11-16 21:38:59 +0100

    Swagger documentation

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 import os
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SWAGGER'] = {
+        'title': 'Energy API',
+        'uiversion': 3
+    }
+
+    Swagger(app, template_file="swagger_spec.yml")
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
     app.config['DEBUG'] = os.environ.get('FLASK_DEBUG') == '1'
